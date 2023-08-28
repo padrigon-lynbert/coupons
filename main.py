4d3a99a208526b33d36728eeebf511d8b346c210
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2023-08-28 22:50:31 +0800

    delete_after.py deleted

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-class Coupon:
-    def discount(self, percentage):
-        pass
+products = {"Cappucino": 100, "Choco": 60, "Black Forest": 70}
+
+order = [70, 60, 60, 100, 100]
+
+def get_order():
+    pass
+
+
+
+
+price = sum(order)
+
+def get_voucher():
+    voucher = input("Voucher: ")
+    return voucher
     
     
